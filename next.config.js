6528d5d0257219d595cb4de2442aa6ddac607755
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-02-19 09:59:57 -0400

    First Stack

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withSass = require('@zeit/next-sass')
+
+module.exports = withSass({
+  webpack(config) {
+    config.plugins = config.plugins.filter(plugin => {
+      if (plugin.constructor.name === 'ForkTsCheckerWebpackPlugin') return false;
+      return true;
+    })
+    return config
+  }
+})
---
+++ src/screens/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = (props) => {
+  return(
+    <span> Home </span>
+  )
+}
+
+export default Home
