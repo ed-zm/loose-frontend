713149c299e697904b90947dfa08a9db4a08b348
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-03-25 01:20:56 -0400

    change port to 80 on production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next = require('next');
 const cookieParser = require('cookie-parser'); // require cookie-parser
 
 const dev = process.env.NODE_ENV !== 'production';
+const PORT = dev ? 3000 : 80
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -15,9 +16,9 @@ app.prepare()
         return handle(req, res);
     });
 
-    server.listen(3000, (err) => {
+    server.listen(PORT, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
+        console.log(`> Ready on http://localhost:${PORT}`);
     });
 })
 .catch((ex) => {
