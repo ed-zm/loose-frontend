76a3843656108e5b7f62afb8aebcd3cef4860b53
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-07-20 22:43:38 -0400

    update and delete comment

--- src/screens/Dashboard/Task/components/Comments/index.tsx
+++ src/screens/Dashboard/Task/components/Comments/index.tsx
@@ -1,26 +1,18 @@
 import React from "react";
-import moment from "moment";
+
 import useTaskComments from "loose-components/src/screens/Dashboard/Task/components/Comments";
 import TextAreaMD from "../../../../../components/TextAreaMD";
 import Button from "../../../../../components/Button";
+import Comment from "./Comment";
 import "./index.scss";
 
 const Comments = ({ task }) => {
-  const { data, comment, setComment, creatingComment, onCreateComment, setMentions } = useTaskComments({ task });
+  const { data, comment, setComment, creatingComment, onCreateComment, setMentions } = useTaskComments({
+    task,
+  });
   return (
     <div className="task-comments">
-      {data &&
-        data.comments &&
-        data.comments.map(({ id, user: { firstName, lastName }, text, createdAt }) => (
-          <div key={id} className="Box">
-            <div className="Box-header Box-header--gray">
-              <h3 className="Box-title">
-                {firstName} {lastName}
-              </h3>
-            </div>
-            <div className="Box-body">{text}</div>
-          </div>
-        ))}
+      {data && data.comments && data.comments.map((comment) => <Comment comment={comment} task={task} />)}
       <TextAreaMD placeholder="description" value={comment} onChange={(e) => setComment(e.target.value)} />
       <Button onClick={onCreateComment} disabled={creatingComment}>
         comment
